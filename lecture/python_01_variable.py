38d0b6aec7871a8275d282e2e274a75ec5a174fa
zzangzang <134247044+zzangzang@users.noreply.github.com>
2023-09-08 12:53:51 +0900

    Add files via upload

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,10 @@
+# 주석(comment): 단순 메모, 개발 실행X
+
+#출력 함수(print)
+# - ()안의 값을 출력
+# - 변수 값 확인 용도 또는 메세지 출력 용도
+print("hello python")
+
+# 문자열 타입(string type)
+# - python: '', "" -> string
+# - c, java: ''(char), ""(string)
